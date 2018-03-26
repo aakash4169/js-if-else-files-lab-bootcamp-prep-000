7427a6f25be46c9c5ac00c7e9ba6d8d7d79815a3
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-03-26 07:08:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var height = "74"
 if(name==="Susan"){
   console.log("name is correc");
 }
+else {
+    console.log("Expected the name to be Susan")
+}
 // Don't worry about this
 module.exports = { name, height
 }
