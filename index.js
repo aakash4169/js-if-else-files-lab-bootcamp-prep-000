c4ad953630e79b90799eb0b9b5a01636f692343c
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-03-26 07:09:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var name = "Susan"
 var height = "74"
 
-if(name==="Susan"){
-  console.log("name is correc");
-}
-else {
-    console.log("Expected the name to be Susan")
-}
+
 // Don't worry about this
 module.exports = { name, height
 }
